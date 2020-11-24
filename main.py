bc08b56becc663ed7873c33f11250649342a9e00
artem-mls <54440303+artem-mls@users.noreply.github.com>
2020-11-24 17:05:50 +0500

    Изменения прямо в GitHub'е

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 def main():
-    # 123
     print('My first git program')
+    print('And I change it every day')
+    print('Again')
+    print('UFO came and added this line')
 
 
 if __name__ == '__main__':
